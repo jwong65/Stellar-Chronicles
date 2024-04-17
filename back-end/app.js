21e30afd7450fcb0208e65014f08d7807d90184e
jwong65 <jason.wong6507@gmail.com>
2024-04-17 14:47:50 -0400

    Added app.js for testing on platform

--- back-end/app.js
+++ back-end/app.js
@@ -9,3 +9,13 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.post('/api/game/start', (req, res) => {
+    const gameState = {
+        id: 1,
+        players: ['Player 1', 'Player 2'],
+        currentTurn: 'Player 1',
+    };
+    res.json({ message: 'New game started!', gameState });
+  });
+  
