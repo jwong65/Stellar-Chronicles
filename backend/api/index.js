9c423c1dd2f5f0d3c7a535ad92f87e98dddec3ab
jwong65 <jason.wong6507@gmail.com>
2024-04-19 13:47:29 -0400

    Removed other directories, testing backend

---
+++ backend/api/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors())
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from Express!' });
+  });
+app.get('/api/game/start', (req, res) => {
+  res.json({message: 'Game already started!'});
+});
+  
+app.post('/api/game/start', (req, res) => {
+    const gameState = {
+        id: 1,
+        players: ['Player 1', 'Player 2'],
+        currentTurn: 'Player 1',
+    };
+    res.json({ message: 'New game started!', gameState: gameState });
+  });
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+  
