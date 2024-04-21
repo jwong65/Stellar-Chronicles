2dd881152114724d08e071db74c809066613105d
jwong65 <jason.wong6507@gmail.com>
2024-04-21 16:14:22 -0400

    Added images to cards and placeholder positoning on animation

--- front-end/cardgame/src/components/common/Card.js
+++ front-end/cardgame/src/components/common/Card.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import './card.css'
 
-export default function Card() {
+export default function Card({value, effect}) {
   return (
-    <div className='card'>Card</div>
+    <div 
+      className={`card`}
+    >
+      <div className='sprite-container'></div>
+      Card: {value} and {effect}
+    </div>
   )
 }
--- front-end/cardgame/src/components/game/GameInterface.js
+++ front-end/cardgame/src/components/game/GameInterface.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './interface.css'
 import Card from '../common/Card'
 
 export default function GameInterface() {
-  return (
+    const [player1Hand, setPlayer1Hand] = useState([
+        { id: 1, value: 'Ace', effect: 'Destroy 2 cards' },
+        { id: 2, value: 'Spade', effect:'Restore 2 health'}
+    ])
+
+    return (
     <div id='game-interface'>GameInterface
-        <Card/>
+        <div className='game-board' id= 'playfield' ></div>
+        <div className='player-hand' id='player-hand'>
+        {player1Hand.map(card => (
+          <Card key={card.id} id={card.id} value={card.value} effect={card.effect} />
+        ))}
+        </div>
     </div>
   )
 }
