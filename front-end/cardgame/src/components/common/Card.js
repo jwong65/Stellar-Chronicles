b6814fd956db4fb144e2d5c31335fa313ab46b05
jwong65 <jason.wong6507@gmail.com>
2024-04-21 22:54:11 -0400

    Testing flip effect on the cards and adjusting the styling to keep the text on the card

--- front-end/cardgame/src/components/common/Card.js
+++ front-end/cardgame/src/components/common/Card.js
@@ -3,12 +3,19 @@ import React, { useState } from 'react'
 import './card.css'
 
 export default function Card({value, effect}) {
+  const [flip, setFlip] = useState(false)
   return (
     <div 
-      className={`card`}
-    >
-      <div className='sprite-container'></div>
-      Card: {value} and {effect}
+      className={`card ${flip ? "flip": ""}`} onClick={()=>setFlip(!flip)}
+    > 
+      <div className='front'>
+        <div className='sprite-container'></div>
+        Card: {value} and {effect}
+      </div>
+      <div className='back'>
+        <h2>Back</h2>
+        {/* <img src='../images/Wood Back.jpg' alt='Card Back'/> */}
+      </div>
     </div>
   )
 }
