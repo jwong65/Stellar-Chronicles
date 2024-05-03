4becd81325c740bff5cba8e8f086fe296b2be33d
jwong65 <jason.wong6507@gmail.com>
2024-05-02 21:05:02 -0400

    Developed the hover that will show card information, not yet stylized or complete

--- front-end/cardgame/src/components/common/Card.js
+++ front-end/cardgame/src/components/common/Card.js
@@ -1,15 +1,25 @@
 import React, { useState } from 'react'
 import './card.css'
 
-export default function Card({id, cost, value, effect, handleCardClick}) {
+export default function Card({id, cost, value, effect, handleCardClick, handleCardHover, handleCardLeave}) {
   const [flip, setFlip] = useState(false)
   const handleClick=() =>{
     handleCardClick(2)
+  }
+  const handleMouseEnter =()=>{
+    handleCardHover({id, cost, value, effect})
+    // console.log('Entered')
+  }
+  const handleMouseLeave = ()=>{
+    // console.log('Leave')
+    handleCardLeave();
   }
   return (
     <div 
       className={`card ${flip ? "flip": ""}`}
       onClick={handleClick} 
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave} 
     > 
       <div className='front'>
         <p>Cost: {cost} </p>
--- front-end/cardgame/src/components/game/GameInterface.js
+++ front-end/cardgame/src/components/game/GameInterface.js
@@ -2,21 +2,27 @@ import React, {useEffect, useState} from 'react'
 import './interface.css'
 import Card from '../common/Card'
 import EndTurn from '../common/EndTurn'
-import InfoMessage from './CardMessage'
+import CardMessage from './CardMessage'
 import GameMessage from './GameMessage'
 
 export default function GameInterface({player1Hand}) {
   const maxHealth = 15
   const [isPlayerTurn, setIsPlayerTurn] = useState(true)
   const [enemyHealth, setEnemyHealth] = useState(maxHealth)
+  const [cardInfo, setCardInfo] = useState({})
 
   const handleCardClick = (card)=>{
     if(card.type === 'Damage'){
       setEnemyHealth(prevHealth =>prevHealth - card.value)
     }
-} 
+  }
+  const handleCardHover = (card)=>{
+    setCardInfo(card)
+  } 
+  const handleCardLeave=()=>{
+    setCardInfo({})
+  }
 const healthPercentage = (enemyHealth / maxHealth) * 100;
-console.log(healthPercentage)
     return (
     <div id='game-interface'>
         <div className='enemy-health' id='enemyhealth'>
@@ -26,7 +32,7 @@ console.log(healthPercentage)
           </div>
         </div>
         <div id='playarea'>
-          <InfoMessage />
+          <CardMessage cardInfo={cardInfo}/>
           <div className='game-board' id= 'playfield' ></div>  
           <div className='end-turn-container'>
             <EndTurn isPlayerTurn={isPlayerTurn} />
@@ -35,7 +41,11 @@ console.log(healthPercentage)
         <GameMessage />
         <div className='player-hand' id='player-hand'>
         {player1Hand.map(card => (
-          <Card key={card.id} id={card.id} value={card.value} effect={card.effect} cost={card.cost} type={card.type} handleCardClick={()=>handleCardClick(card)}/>
+          <Card key={card.id} id={card.id} value={card.value} effect={card.effect} cost={card.cost} type={card.type} 
+            handleCardClick={() => { handleCardClick(card); }} 
+            handleCardHover={handleCardHover}
+            handleCardLeave={handleCardLeave}
+          />
         ))}
         </div>
     </div>
