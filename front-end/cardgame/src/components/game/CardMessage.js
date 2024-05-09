d88cbc46f0c3d77dcea4f6affba43956588b003e
jwong65 <jason.wong6507@gmail.com>
2024-05-08 22:55:12 -0400

    Changed the message-info to match the color of card-message

--- front-end/cardgame/src/components/game/CardMessage.js
+++ front-end/cardgame/src/components/game/CardMessage.js
@@ -2,16 +2,14 @@ import React from 'react'
 import './info.css'
 
 export default function CardMessage({ cardInfo }) {
-// console.log(cardInfo)
 const { id, cost, value, effect } = cardInfo;
-// console.log(cardInfo.cost, "Cost")
 return (
 <div className='card-message'>Card Information
 {cardInfo && Object.keys(cardInfo).length > 0 && (
 <>
-<div className='cardcost'>{cost !== undefined ? `Cost: ${cost}` : ''}</div>
-<div className='cardname'>{value !== undefined ? `Card Name: ${value}` : ''}</div>
-<div className='cardeffect'>{effect !== undefined ? `Card Effect: ${effect}` : ''}</div>
+  <div className='cardcost'>{cost !== undefined ? `Cost: ${cost}` : ''}</div>
+  <div className='cardname'>{value !== undefined ? `Card Name: ${value}` : ''}</div>
+  <div className='cardeffect'>{effect !== undefined ? `Card Effect: ${effect}` : ''}</div>
 </>
 )}
 </div>
