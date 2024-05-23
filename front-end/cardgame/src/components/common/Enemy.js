3de1a1eb3da1da6659abb7c7c8ba4abd46dc487c
jwong65 <jason.wong6507@gmail.com>
2024-05-23 00:48:57 -0400

    Added enemy distinctions and styling for game-board

--- front-end/cardgame/src/components/common/Enemy.js
+++ front-end/cardgame/src/components/common/Enemy.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 export default function Enemy({enemy, onClick}) {
+    const enemyHealthPercentage = (enemy.health / enemy.maxHealth) * 100;
   return (
-    <div className='enemy' onClick={()=>onClick(enemy)}>
+    <div className='enemy'>
         <div className='enemy-name'>{enemy.name}</div>
         <div className='enemy-health'>Health: {enemy.health}</div>
         <div className='health-bar'>
-          <div className='health-fill' style={{ width: `${(enemy.health / enemy.maxHealth) * 100}%` }}></div>
+          <div className='health-fill' style={{ width: `${enemyHealthPercentage}%` }}></div>
         </div>
     </div>
   )
