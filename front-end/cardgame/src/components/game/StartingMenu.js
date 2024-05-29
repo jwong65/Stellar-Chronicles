3d14924ebec2d544338e7af62757ecd84ff43a96
jwong65 <jason.wong6507@gmail.com>
2024-05-29 11:03:28 -0400

    Removed compababilty message from menu

--- front-end/cardgame/src/components/game/StartingMenu.js
+++ front-end/cardgame/src/components/game/StartingMenu.js
@@ -7,7 +7,7 @@ export default function StartingMenu() {
     <div id='start'>
       <h1>Stellar Chronicles</h1>
       <p>A card game of strategy and chance</p>
-      <h3>This application is currently finding difficulty with card selections in other browsers that are not Firefox. For optimal selection please try using a Firefox browser.</h3>
+      
       <div className='button-container'>
         <button>Start Game</button>
         <Link to="/tutorial">
