86a968560bac488425e73f357dccdcdac58691d7
jwong65 <jason.wong6507@gmail.com>
2024-06-21 17:57:40 -0400

    Added directory for startGame and added 2 components Map and NewGame.js, grid developed for consideration of a map

--- front-end/cardgame/src/components/startGame/Map.js
+++ front-end/cardgame/src/components/startGame/Map.js
@@ -2,19 +2,43 @@ import React from 'react'
 import './map.css'
 
 export default function Map() {
+  const generateMap =(height, width)=>{
+    const map = []
+    const cellType = ['empty', 'enemy']
+
+    for (let i = 0; i < height; i++) {
+      const row = []
+      for (let j = 0; j < width; j++) {
+        const randomIndex= Math.floor(Math.random() * cellType.length)
+        const type = cellType[randomIndex]
+        row.push({type: 'wall'})
+      }
+      map.push(row)
+    }
+    // const pathCells = [
+    //   [0, 5], [1, 5], [2, 4], [2, 5], [2, 6], [3, 4], [4, 4], [5, 4],
+    //   [5, 5], [5, 6], [6, 6], [7, 6], [8, 5], [9, 5]
+    // ];
+
+    // pathCells.forEach(([row, col])=>{
+    //   map[row][col] = {type: 'path'}
+    // })
+
+    return map
+  }
+  const map = generateMap(10, 10)
+  
+
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: `repeat(${width}, 30px)` }}>
-    {map.flat().map((room, index) => (
-      <div
-        key={index}
-        style={{
-          width: '30px',
-          height: '30px',
-          border: '1px solid black',
-          backgroundColor: room.type === 'empty' ? 'grey' : 'white',
-        }}
-      ></div>
-    ))}
-  </div>
+    <div className='map-container'>
+      {map.map((row, rowIndex)=> (
+        row.map((cell, cellIndex)=>
+        <div 
+          key={`${rowIndex}-${cellIndex}`}
+          className={`map-cell map-cell-${cell.type}`}
+          //  map-cell-${cell.type}
+        ></div>
+      )))}
+    </div>
   )
 }
--- front-end/cardgame/src/components/startGame/NewGame.js
+++ front-end/cardgame/src/components/startGame/NewGame.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Map from './Map'
+
+import './map.css'
 
 export default function NewGame() {
   return (
+    <div className='game-container'>
     <div>HELLo</div>
+    
+
+    <Map/>
+    </div>
   )
 }
